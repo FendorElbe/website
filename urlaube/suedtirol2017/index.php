fe3a1a5d9aafd4f0e4e4a47a4f75ccaf4358f172
Rolfus187 <rolfus187@googlemail.com>
2018-12-13 19:14:22 +0100

    script

--- urlaube/suedtirol2017/index.php
+++ urlaube/suedtirol2017/index.php
@@ -59,7 +59,15 @@
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     <!-- custom JS -->
-    <script src="custom.js"></script>
+    <script>
+    $(document).ready(function () {
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+
+    });
+</script>
 
 </body>
 
