7a5b3933c4537c4206f341973642ad14ab420718
Rolfus187 <rolfus187@googlemail.com>
2018-12-14 17:47:08 +0100

    maybe?

--- urlaube/suedtirol2017/xx_xx_xx/index.php
+++ urlaube/suedtirol2017/xx_xx_xx/index.php
@@ -18,7 +18,7 @@
             if($count == 0){
               echo '
               <div class="carousel-item slideimg active">
-                <img class="d-block oversize" src="img/'.$file.'" alt="First slide">
+                <img class="d-block oversize" src="./img/'.$file.'" alt="First slide">
                 <div class="carousel-caption w-100 px-5 d-none d-md-block">
                   <h5>'.$captions_lines[$count++].'</h5>
                   <p>'.$captions_lines[$count++].'</p>
@@ -29,7 +29,7 @@
             else{
               echo '
               <div class="carousel-item slideimg">
-                <img class="d-block oversize" src="img/'.$file.'" alt="First slide">
+                <img class="d-block oversize" src="./img/'.$file.'" alt="First slide">
                 <div class="carousel-caption w-100 px-5 d-none d-md-block">
                   <h5>'.$captions_lines[$count++].'</h5>
                   <p>'.$captions_lines[$count++].'</p>
