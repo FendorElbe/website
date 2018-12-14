d4afd8c0f44fad6558a7184795b5c14c7dd556a4
Rolfus187 <rolfus187@googlemail.com>
2018-12-14 16:19:52 +0100

    trying two folders

---
+++ urlaube/suedtirol2017/zz_zz_zz/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Südtirol 2017</title>
+  </head>
+  <body>
+    DIES IST EIN ANDERER TEST
+  </body>
+</html>
